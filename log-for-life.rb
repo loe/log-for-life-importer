8ecc0e1d6ae82d340a42fb8547fd74464031eaf4
W. Andrew Loe III <andrew@andrewloe.com>
2009-01-20 01:24:50 -0800

    Parsing of Onetouch CSV files.

--- log-for-life.rb
+++ log-for-life.rb
@@ -4,48 +4,55 @@ require 'rubygems'
 require 'activeresource'
 require 'nokogiri'
 require 'time'
+require 'csv'
 
 module LogForLife
-
+  
+  class LogItem < ActiveResource::Base
+    c = YAML::load_file('config.yml')
+    self.site = "https://www.logforlife.com/logs/#{c['id']}/"
+    self.user = c['key']
+  end
+  
   class Importer
-
-    attr_accessor :site, :user, :entries
-
+    
+    attr_accessor :entries
+    
     def initialize
-      # Configuration for logforlife.com, logid and API key.
-      c = YAML::load_file('config.yml')
-      @site = "https://www.logforlife.com/logs/#{c['id']}/"
-      @user = c['key']
-      
       @entries = []
     end
-
+    
     def parse_dexcom_xml(path = 'dexcom/sensor.Export.xml')
-      f = File.open(path)
-      doc = Nokogiri(f.read)
-      f.close
-
+      
+      doc = Nokogiri(File.open(path))
+      
       doc.search('Sensor').each do |s|
         t = Time.parse(s['DisplayTime'])
-        log_item = {}
-        log_item[:glucose_quantity] = s['Value']
-        log_item[:time] = t.strftime("%I:%M %p")
-        log_item[:date] = t.strftime("%Y-%m-%d")
-
-        @entries << log_item
+        i = {:glucose_quantity => s['Value'],
+             :date => t.strftime("%Y-%m-%d"),
+             :time => t.strftime("%I:%M %p")}
+        
+        @entries << i
+      end
+    end
+    
+    def parse_onetouch_csv(path = 'onetouch/Export_1202009.csv')
+      CSV::Reader.parse(File.open(path)) do |row|
+        i = {:glucose_quantity => row[3],
+             :date => Date.parse(row[1]),
+             :time => Time.parse(row[2]).strftime("%I:%M %p")}
+        
+        @entries << i
       end
     end
     
     def submit
       @entries.each do |e|
-        response = LogItem.post(:bulk_create, :log_item => e, :site => @site, :user => @user)
+        response = LogItem.post(:bulk_create, :log_item => e)
         puts response.to_yaml
       end
     end
-
-  end
-
-  class LogItem < ActiveResource::Base
+    
   end
 
 end
