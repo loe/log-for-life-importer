7249bf486fba3fe21d193c8bb8e2118904b62403
W. Andrew Loe III <andrew@andrewloe.com>
2009-01-20 00:47:00 -0800

    Rearranging into a Module to facilitate parsing other types of output.

--- log-for-life.rb
+++ log-for-life.rb
@@ -5,36 +5,47 @@ require 'activeresource'
 require 'nokogiri'
 require 'time'
 
-c = YAML::load_file('config.yml')
+module LogForLife
 
-class LogItem < ActiveResource::Base
-  self.site = "https://www.logforlife.com/logs/#{c['id']}/"
-  self.user = c['key']
+  class Importer
+
+    attr_accessor :site, :user, :entries
+
+    def initialize
+      # Configuration for logforlife.com, logid and API key.
+      c = YAML::load_file('config.yml')
+      @site = "https://www.logforlife.com/logs/#{c['id']}/"
+      @user = c['key']
+      
+      @entries = []
+    end
+
+    def parse_dexcom_xml(path = 'dexcom/sensor.Export.xml')
+      f = File.open(path)
+      doc = Nokogiri(f.read)
+      f.close
+
+      doc.search('Sensor').each do |s|
+        t = Time.parse(s['DisplayTime'])
+        log_item = {}
+        log_item[:glucose_quantity] = s['Value']
+        log_item[:time] = t.strftime("%I:%M %p")
+        log_item[:date] = t.strftime("%Y-%m-%d")
+
+        @entries << log_item
+      end
+    end
+    
+    def submit
+      @entries.each do |e|
+        response = LogItem.post(:bulk_create, :log_item => e, :site => @site, :user => @user)
+        puts response.to_yaml
+      end
+    end
+
+  end
+
+  class LogItem < ActiveResource::Base
+  end
+
 end
-
-f = File.open('sensor.Export.xml')
-doc = Nokogiri(f.read)
-f.close
-
-doc.search('Sensor').each do |s|
-  t = Time.parse(s['DisplayTime'])
-  log_item = {}
-  log_item[:glucose_quantity] = s['Value']
-  log_item[:time] = t.strftime("%I:%M %p")
-  log_item[:date] = t.strftime("%Y-%m-%d")
-  
-  response = LogItem.post(:bulk_create, :log_item => log_item)
-  
-  puts response.to_yaml
-end
-
-# 25.times do
-#   response = LogItem.post(:bulk_create, :log_item => {
-#    :glucose_quantity => rand(300),
-#    :time => "8:30 AM",
-#    :date => Date.today
-#   })
-#   
-#   puts response.to_yaml
-# end
-
